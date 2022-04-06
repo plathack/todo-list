f94562336ac28850c6298ea2699f8fbb00eff606
plathack <EfimcevIA@zsniigg.ru>
2022-04-06 12:51:08 +0500

    sidebar and main-content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import MainContent from './components/MainContent/MainContent';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className='App'>
       <div className='container'>
-          
+          <Sidebar />
+          <MainContent />
       </div>
     </div>
   );
